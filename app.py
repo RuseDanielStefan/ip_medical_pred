03df9ebcc963cc5d0a1ea31c17c1ee978ca4c8ae
Gabriel Agache <g_agache@yahoo.com>
2019-04-20 21:48:43 +0300

    heroku test

--- app.py
+++ app.py
@@ -46,5 +46,10 @@ def get_result():
     return predict.classify(data, k=30)
 
 
+@app.route('/hello')
+def hello():
+    return 'hello from hello'
+
+
 if __name__ == '__main__':
-    app.run(host='localhost', port=5000, debug=True)
+    app.run()
